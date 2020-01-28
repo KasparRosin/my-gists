fb80458d6be7634ad51e8ef5765f8bdc6f307df9
Kaspar Rosin <kaspar.rosin@tptlive.ee>
2020-01-28 22:00:06 +0200

    array-to-object conversion

---
+++ snippets/javascript/README.md
@@ -0,0 +1,3 @@
+# Javascript snippets
+
+### 1. [Convert array of objects into a object](array-to-objects.js)
---
+++ snippets/javascript/array-to-object.js
@@ -0,0 +1,29 @@
+// <-- FROM -->
+const example_json = [
+  {
+    Title_1: {
+      name: "john",
+      age: 19
+    }
+  },
+  {
+    Title_2: {
+      name: "jane",
+      age: 26
+    }
+  }
+];
+
+const result = example_json.reduce((obj, item) => {
+  Object.entries(item).map(([key, value]) => {
+    return (obj[key] = value);
+  });
+  return obj;
+}, {});
+console.log(result);
+
+// <-- OUTPUT -->
+/*[
+    Title_1: [OBJECT],
+    Title_2: [OBJECT]
+]*/
