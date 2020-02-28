29e2983ff79ffc511ae9be7e0025cc35fe4264b3
Kaspar Rosin <kaspar.rosin@tptlive.ee>
2020-02-28 22:25:18 +0200

    add logging option (for testing)

--- javascript/is-url.js
+++ javascript/is-url.js
@@ -1,3 +1,8 @@
+const prompt = require("prompt-sync")();
+
 function isURL(str) {
   return /^(?:\w+:)?\/\/([^\s\.]+\.\S{2}|localhost[\:?\d]*)\S*$/.test(str);
 }
+
+const url = prompt("Insert url: ");
+console.log(`${url} is ${isURL(url) ? "valid" : "invalid"} url`);
